b4f945a6d496aa6726bad4d88a8b149349129b92
WizVader <126302304+WizVader@users.noreply.github.com>
2024-07-21 00:44:35 +1200

    Problem 392 soln using two pointers (?)

--- two-pointers/problem392.py
+++ two-pointers/problem392.py
@@ -2,20 +2,19 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         s_length = len(s)
         t_length = len(t)
-        lesser_length = s_length if s_length < t_length else t_length
-        i, j = 0, 0
-        count = 0
+        i, j, count = 0, 0, 0
+
         while i < s_length and j < t_length:
             if s[i] == t[j]:
                 count += 1
                 i += 1
                 j += 1
             else:
-                i += 1
-        if count == lesser_length:
+                j += 1
+
+        if count == s_length:
             return True
         else:
             return False
-
 solution = Solution()
-solution.isSubsequence("abc", "ahbgdc")
+print(solution.isSubsequence("abc", "ahbgdc"))
